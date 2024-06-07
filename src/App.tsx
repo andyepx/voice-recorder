804c668f4afdb92c1658ecf06589df7b977bdd94
Andrea Epifani <andrea@andreaepifani.com>
2024-06-07 11:19:29 +1000

    Basic recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,132 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const DB_NAME = "recordings";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+    const [recordings, setRecordings] = useState<{ label: string, value: string }[]>([]);
+    const [recordingState, setRecordingState] = useState<RecordingState>("inactive");
+
+    useEffect(() => {
+        const request = window.indexedDB.open(DB_NAME);
+        request.onsuccess = (e) => {
+            const db = request.result;
+            const transaction = db.transaction([DB_NAME]);
+            const query = transaction.objectStore(DB_NAME).openCursor();
+            query.onsuccess = function (ev) {
+                const r = this.result;
+                if (!r) {
+                    db.close();
+                    return;
+                }
+                setRecordings(p => [...p, {
+                    value: window.URL.createObjectURL(r.value),
+                    label: String(r.key),
+                }]);
+                r.continue();
+            };
+        };
+    }, []);
+
+    const startRecording = () => {
+        if (!mediaRecorder) {
+            navigator.mediaDevices
+                .getUserMedia({audio: true})
+                .then((stream) => {
+                        const recorder = new MediaRecorder(stream)
+
+                        let chunks: Blob[] = [];
+                        recorder.ondataavailable = (e) => {
+                            chunks.push(e.data);
+                        };
+
+                        recorder.onstop = (e) => {
+                            console.log("recorder stopped");
+
+                            const clipName = new Date().toString();
+                            const blob = new Blob(chunks, {type: "audio/ogg; codecs=opus"});
+                            chunks = [];
+
+                            const request = window.indexedDB.open(DB_NAME);
+
+                            request.onupgradeneeded = (event: any) => {
+                                const db = event.target.result;
+                                if (!db.objectStoreNames.contains(DB_NAME)) {
+                                    db.createObjectStore(DB_NAME);
+                                }
+                            };
+
+                            request.onsuccess = (e) => {
+                                const db = request.result;
+                                const transaction = db.transaction([DB_NAME], "readwrite");
+                                transaction.objectStore(DB_NAME).put(blob, clipName).onsuccess
+                                transaction.objectStore(DB_NAME).get(clipName).onsuccess = function (event) {
+                                    const r = (event.target! as any).result;
+                                    setRecordings(p => [...p, {
+                                        value: window.URL.createObjectURL(r),
+                                        label: clipName,
+                                    }]);
+
+                                    db.close();
+                                };
+                            };
+
+                            stream.getTracks().forEach(t => t.stop());
+                            setMediaRecorder(null);
+                        };
+
+                        recorder.start();
+                        setRecordingState(recorder.state);
+
+                        setMediaRecorder(recorder);
+                    }
+                );
+        }
+    }
+
+    const stopRecording = () => {
+        if (!mediaRecorder) return;
+        mediaRecorder.stop();
+        setRecordingState(mediaRecorder.state);
+    }
+
+    const pauseResumeRecording = () => {
+        if (!mediaRecorder) return;
+
+        if (recordingState === "paused") {
+            mediaRecorder.resume();
+            setRecordingState(mediaRecorder.state);
+        } else {
+            mediaRecorder.pause();
+            setRecordingState(mediaRecorder.state);
+        }
+    }
+
+    return (
+        <>
+            {mediaRecorder ? <div>
+                <button onClick={stopRecording}>Stop</button>
+                <button onClick={pauseResumeRecording}>
+                    {recordingState === "paused" ? "Resume" : "Pause"}
+                </button>
+            </div> : <div>
+                <button onClick={startRecording}>Record</button>
+            </div>}
+            <div>
+                <br/>
+                {recordings
+                    .sort((a, b) => b.label.localeCompare(a.label))
+                    .map((r, ix) => (
+                        <div key={r.label}>
+                            {r.label}
+                            <br/>
+                            <audio key={ix} controls src={r.value}/>
+                        </div>
+                    ))}
+            </div>
+        </>
+    )
 }
 
 export default App
